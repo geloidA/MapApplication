76dfcb1119c2cc76b63ae3244fddf38edd407940
Aleksander <alexkolpak2003@gmail.com>
2022-12-29 10:44:48 +0500

    fix

---
+++ Views/Controls/MenuPanel.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace map_app.Views.Controls;
+
+public partial class MenuPanel : UserControl
+{
+    public MenuPanel()
+    {
+        InitializeComponent();
+    }
+}
